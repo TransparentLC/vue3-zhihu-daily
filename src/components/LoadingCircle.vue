9d51a7a5c95c156b2f50c74e1916180acd6e63b8
✨小透明・宸✨ <1272742391@qq.com>
2020-09-18 22:49:45 +0800

    Update for gh-pages

--- src/components/LoadingCircle.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div style="margin:32px 0">
-        <div class="loading loading-lg my-2"></div>
-        <div class="text-center text-primary">
-            <small>Loading</small>
-        </div>
-    </div>
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-import axios from 'axios';
-// axios.defaults.baseURL = 'https://zhihu-daily.transparentlc.workers.dev/api/4/';
-axios.defaults.baseURL = '/api/4/';
-
-createApp(App).use(router).mount('#app');
